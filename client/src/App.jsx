d75bad2c38697dc76c976d453afb5172c2f4c24d
Aayush-Bisht1 <aayushbisht205@gmail.com>
2025-01-05 21:02:12 +0530

    protected route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,30 @@ import React, { useState, useEffect } from 'react';
 import AuthPage from './pages/authPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import axios from 'axios';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const fetchUser = async () => {
+    try {
+      const response = await axios.get("https://frs-task.onrender.com/api/auth/user", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      setUser(response.data.user);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    fetchUser();  
+  },[]);
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<AuthPage />}/>
-        <Route path='/dashboard' element={<Dashboard />}/>
+        <Route path='/' element={user ? <Dashboard /> : <AuthPage />}/>
+        <Route path='/dashboard' element={user ? <Dashboard />: <AuthPage />}/>
       </Routes>
     </Router>
   )
